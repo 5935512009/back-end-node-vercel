01ec351531b9a1cc462b7ccc4df3fe5b97070eb4
5935512009 <Pasut2541@hotmail.com>
2024-05-03 12:16:12 +0700

    text:{index.js}

--- index.js
+++ index.js
@@ -1,40 +1,12 @@
 // bay-resume-back-end/index.js
 
+// เปลี่ยนรูปแบบ type เป็น module
 const express = require('express');
-const mongoose = require('mongoose');
 
-const app = express();
+const app = express()
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://admin:5PWZZLV.i_u5eqT@cluster0.xn0erow.mongodb.net/resumetodolist?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
-
-// Define Schema
-const todoSchema = new mongoose.Schema({
-  id: String,
-  description: String
+app.use("/",(req,res)=>{
+  res.send("server is running");
 });
-
-// Define Model
-const Todo = mongoose.model('Todo', todoSchema, 'todolistcollection');
-
-// Define API endpoint to get todos
-app.get('/todos', async (req, res) => {
-  try {
-    const todos = await Todo.find();
-    res.json(todos);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Start server
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT =5001;
+app.listen(PORT, console.log(`server is running ${PORT}`));
